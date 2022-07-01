a2b097a1bf9686ed3565efdb0b7e032913f63939
AmmarrBN <104218064+AmmarrBN@users.noreply.github.com>
2022-07-01 16:39:30 +0700

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -1,5 +1,5 @@
-# Encoded By Py-Fuscate
-# https://github.com/Sl-Sanda-Ru/Py-Fuscate
+# Encrypt By AmmarBN
+# https://github.com/AmmarrBN
 # Make Sure You're Running The Program With python3.10 Otherwise It May Crash
 # To Check Your Python Version Run "python -V" Command
 try:
